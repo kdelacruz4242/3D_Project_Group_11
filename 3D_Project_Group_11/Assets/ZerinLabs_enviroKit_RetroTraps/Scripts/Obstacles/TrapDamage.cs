b033ed07b648d0113beb119261e084a040c8d8a5
Abigail <adupaya7389@sdsu.edu>
2026-05-10 19:59:26 -0700

    Added Axe Trap on the Wall

--- 3D_Project_Group_11/Assets/ZerinLabs_enviroKit_RetroTraps/Scripts/Obstacles/TrapDamage.cs
+++ 3D_Project_Group_11/Assets/ZerinLabs_enviroKit_RetroTraps/Scripts/Obstacles/TrapDamage.cs
@@ -2,49 +2,12 @@ using UnityEngine;
 
 public class TrapDamage : MonoBehaviour
 {
-    BoxCollider trapCollider;
-    Transform spikes;
-
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        trapCollider = GetComponent<BoxCollider>();
-
-        foreach (Transform child in GetComponentsInChildren<Transform>())
+        PlayerHealth health = other.GetComponent<PlayerHealth>();
+        if (health != null)
         {
-            if (child.name == "decoTrap_spikesfloor_spikes")
-            {
-                spikes = child;
-                break;
-            }
-        }
-    }
-
-    void Update()
-    {
-        if (spikes != null && trapCollider != null)
-        {
-            // check position AND scale on all axes
-            Debug.Log("Pos: " + spikes.localPosition + " Scale: " + spikes.localScale);
-            
-            // enable when scale Y is above 0.5 (spikes growing up)
-            trapCollider.enabled = spikes.localScale.z > -0.5f;
-        }
-
-        if (trapCollider != null && trapCollider.enabled)
-        {
-            Collider[] hits = Physics.OverlapBox(
-                trapCollider.bounds.center,
-                trapCollider.bounds.extents
-            );
-
-            foreach (Collider hit in hits)
-            {
-                PlayerHealth health = hit.GetComponent<PlayerHealth>();
-                if (health != null)
-                {
-                    health.TakeDamage();
-                }
-            }
+            health.TakeDamage();
         }
     }
 }
