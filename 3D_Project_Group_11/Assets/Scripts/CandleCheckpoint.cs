2b06c3b93fa15dc95dded25672577db8f3464665
Kaye-Angeli <kdelacruz4242@sdsu.edu>
2026-05-12 20:09:34 -0700

    updated

--- 3D_Project_Group_11/Assets/Scripts/CandleCheckpoint.cs
+++ 3D_Project_Group_11/Assets/Scripts/CandleCheckpoint.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
-public class CandleCheckpoint
+public class CandleCheckpoint : MonoBehaviour
 {
-    
+    public GameObject candleLight;
+    public Transform spawnPoint;
+
+    private bool activated = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (activated) return;
+
+        if (other.CompareTag("Player"))
+        {
+            activated = true;
+
+            if (candleLight != null)
+                candleLight.SetActive(true);
+
+           other.transform.position = spawnPoint.position;
+        }
+    }
 }
